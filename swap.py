8866df05eeec54285f3ffeb61b33ef20c4c0591e
Imani <46060122+imani-everette@users.noreply.github.com>
2019-02-01 13:56:55 -0500

    Create swap.py
    Modified the above program so that what’s in the variable word1 is now in the variable word2 and what’s in variable word2 is now in variable word1. I was not allowed to remove any lines of code from this program. I could only add assignment statements between lines 3 and 6 to get the desired result.

---
+++ swap.py
@@ -0,0 +1,10 @@
+word1 = input("Enter word #1: ")
+word2 = input("Enter word #2: ")
+
+# assignment statement
+word1,word2=word2,word1
+
+print(
+print("Result of swapping variables.")
+print("word #1:", word1)
+print("word #2:", word2)
