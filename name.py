20688a0227dd3ef73a3d12bb45f1ed317d1f1ec8
Imani <46060122+imani-everette@users.noreply.github.com>
2019-02-01 09:44:26 -0500

    Create name.py
    A program that asks for a person’s first name, then middle, and then last. It then greets the person using their full name.

---
+++ name.py
@@ -0,0 +1,7 @@
+#Write a program called name.py that asks for a person’s first name, then middle, and then last. It should then greet the person using their full name. Hint: You will need to use variables when writing this program.
+
+first_name = input('Enter first name:')
+middle_name = input('Enter middle name:')
+last_name = input('Enter last name:')
+print(' ')
+print(f'Hi {first_name} {middle_name} {last_name}!')
